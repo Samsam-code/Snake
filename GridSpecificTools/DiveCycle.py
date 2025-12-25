727e4b6ee95ec1ee28326722581c6d4af13de205
Samuel <samuel.avril@ens-lyon.fr>
2025-12-25 17:25:11 +0100

    add asym_dive_cycle

--- GridSpecificTools/DiveCycle.py
+++ GridSpecificTools/DiveCycle.py
@@ -25,6 +25,33 @@ def dive_cycle_even(n,dive_lengths):
 
     return path
 
+def asym_dive_cycle_even(n,left_dives, right_dives):
+    # There should be m//2 -1 dives
+    path = []
+
+    # Dives from left side
+    nx = n
+    for l in left_dives:
+        path.extend(range(nx, nx+l+1))
+        nx+=n
+        path.extend(range(nx+l, nx-1, -1))
+        nx+=n
+
+    # Go to right side along the bottom edge
+    path.extend(range(nx, nx+n))
+
+    # Dives from right side
+    for l in reversed(right_dives):
+        path.extend(range(nx-1, nx-l-2, -1))
+        nx-=n
+        path.extend(range(nx-l-1, nx))
+        nx-=n
+
+    # Go to left side along the top edge
+    path.extend(range(n-1, -1, -1))
+
+    return path
+
 def double_comb_cycle(m, n):
     return dive_cycle_even(n, [n//2-1]*(m//2-1))
 
