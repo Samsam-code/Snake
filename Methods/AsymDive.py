e2fbc9b7ccbc5be4b46f6975f510341de87e2b4c
Samuel <samuel.avril@ens-lyon.fr>
2026-01-09 20:40:40 +0100

    AsymDive: optimize case where apple is on loop but on the opposite side og the grid

--- Methods/AsymDive.py
+++ Methods/AsymDive.py
@@ -28,7 +28,7 @@ class GridSolver_AsymDive():
         if self.new_loop:
             self.update_side()
             if self.snake_length < self.cutoff_length:
-                self.decide_dive_lengths(apple)
+                self.decide_dive_lengths(divmod(apple, self.n))
             else:# self.snake_length == self.cutoff_length:
                 lim = (self.head_coord[0]-1)//2 + 1 - self.is_left
                 if self.is_left:
@@ -91,7 +91,7 @@ class GridSolver_AsymDive():
             elif head_y == self.n-1:
                 self.is_left = False
                 return
-            elif head_x == self.snake[0]//self.n:
+            elif head_x == self.snake[0]//self.n and abs(self.head-self.snake[0]) == self.snake_length-1:
                 self.is_left = None
                 return
         
@@ -99,32 +99,32 @@ class GridSolver_AsymDive():
         # to the index of the bottom right corner in the loop
         self.is_left = self.idx_head<=self.idx_bottom_right
     
-    def decide_dive_lengths(self, apple):
+    def decide_dive_lengths(self, apple_coords):
         """ Based on the position of the snake and the apple, find a good dive cycle to get to the apple quickly """
         head_x, head_y = self.head_coord
-        apple_x, apple_y = divmod(apple, self.n)
+        apple_x, apple_y = apple_coords
         
         # Find out whether we will reach apple from left side or not
         if apple_y == 0: # Apple on left edge
-            reach_apple_from_left_side = True
+            apple_is_left = True
         elif apple_y == self.n -1: # Apple on right edge
-            reach_apple_from_left_side = False
+            apple_is_left = False
         elif head_x > apple_x: # Snake is below apple
-            reach_apple_from_left_side = False
+            apple_is_left = False
         elif head_x < apple_x: # Snake is above apple
-            reach_apple_from_left_side = True
+            apple_is_left = True
         elif head_x%2 == 0: # Snake and apple aligned and snake goes left
-            reach_apple_from_left_side = self.is_left and head_y>apple_y
+            apple_is_left = self.is_left and head_y>apple_y
         else: # Snake and apple aligned and snake goes right
-            reach_apple_from_left_side = self.is_left or head_y>apple_y
+            apple_is_left = self.is_left or head_y>apple_y
 
         idx_dive_apple = (apple_x-1)//2
-        self.forget_some_dives(idx_dive_apple, reach_apple_from_left_side)
+        self.forget_some_dives(apple_coords, apple_is_left)
         
         idx_dive_head = (head_x-1)//2
         if self.is_left is None:
             # The snake forms a line so we decide to orient the snake in the fastest way to reach the apple
-            self.is_left = apple>self.head
+            self.is_left = apple_is_left
             if self.is_left:
                 self.left_dives[idx_dive_head] = head_y
             else:
@@ -134,15 +134,28 @@ class GridSolver_AsymDive():
             # Apple is on an edge => stick to the edges
             return
         
+        # Apple is inside the grid
         r_dive_apple = self.n - apple_y - 1
-        if self.left_dives[idx_dive_apple]<apple_y and self.right_dives[idx_dive_apple]< r_dive_apple:
-            # Apple is inside the grid
-            if reach_apple_from_left_side:
-                self.left_dives[idx_dive_apple] = apple_y
-            else:
-                self.right_dives[idx_dive_apple] = r_dive_apple
+        if apple_is_left:
+            if self.left_dives[idx_dive_apple]<apple_y:
+                if self.right_dives[idx_dive_apple]< r_dive_apple:
+                    self.left_dives[idx_dive_apple] = apple_y
+                elif self.snake[0]//self.n == apple_x and self.snake[0]<apple_x*self.n+apple_y:
+                    pass
+                else:
+                    self.left_dives[idx_dive_apple] = apple_y
+                    self.right_dives[idx_dive_apple] = r_dive_apple-1
+        else:
+            if self.right_dives[idx_dive_apple]< r_dive_apple:
+                if self.left_dives[idx_dive_apple]< apple_y:
+                    self.right_dives[idx_dive_apple] = r_dive_apple
+                elif self.snake[0]//self.n == apple_x and self.snake[0]>apple_x*self.n+apple_y:
+                    pass
+                else:
+                    self.right_dives[idx_dive_apple] = r_dive_apple
+                    self.left_dives[idx_dive_apple] = apple_y-1
 
-    def forget_some_dives(self, idx_apple, reach_apple_from_left_side):
+    def forget_some_dives(self, apple_coords, apple_is_left):
         """ Delete as much dives as possible in a safe way """
         len_dives = self.m//2 -1
         if self.is_left is None or self.loop is None:
@@ -164,16 +177,34 @@ class GridSolver_AsymDive():
         if margin<=0:
             return
         
-        idx_head = (self.head_coord[0]-1)//2
+        head_x = self.head_coord[0]
+        apple_x = apple_coords[0]
+        idx_dive_head = (self.head_coord[0]-1)//2
+        idx_dive_apple = (apple_coords[0]-1)//2
         if self.is_left:
-            if reach_apple_from_left_side:
-                reduce_dives(self.left_dives, margin, idx_head+1, idx_apple)
+            if apple_is_left:
+                end_offset = 1-apple_x%2
+                # if apple_x<head_x:
+                #     self.left_dives[idx_dive_head+1:] = [0]*(len_dives-idx_dive_head-1)
+                #     self.right_dives = [0]*len_dives
+                #     self.left_dives[:idx_dive_head+end_offset] = [0]*(idx_dive_head+end_offset)
+                #     return
+                
+                reduce_dives(self.left_dives, margin, idx_dive_head+1, idx_dive_apple+end_offset)
             else:
-                margin = reduce_dives(self.left_dives, margin, idx_head+1, len_dives)
-                reduce_dives(self.right_dives, margin, len_dives-1, idx_apple,-1)
+                end_offset = apple_x%2
+                margin = reduce_dives(self.left_dives, margin, idx_dive_head+1, len_dives)
+                reduce_dives(self.right_dives, margin, len_dives-1, idx_dive_apple-end_offset,-1)
         else:
-            if reach_apple_from_left_side:
-                margin = reduce_dives(self.right_dives, margin, idx_head-1, -1, -1)
-                reduce_dives(self.left_dives, margin, 0,  idx_apple)
+            if apple_is_left:
+                end_offset = 1-apple_x%2
+                margin = reduce_dives(self.right_dives, margin, idx_dive_head-1, -1, -1)
+                reduce_dives(self.left_dives, margin, 0,  idx_dive_apple+end_offset)
             else:
-                reduce_dives(self.right_dives, margin, idx_head-1, idx_apple,-1)
+                end_offset = apple_x%2
+                # if apple_x>head_x:
+                #     self.right_dives[:idx_dive_head] = [0]*(idx_dive_head)
+                #     self.left_dives = [0]*len_dives
+                #     self.right_dives[idx_dive_apple-end_offset+1:] = [0]*(len_dives-idx_dive_apple+end_offset-1)
+                #     return
+                reduce_dives(self.right_dives, margin, idx_dive_head-1, idx_dive_apple-end_offset,-1)
